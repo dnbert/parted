de94860ee35bc2baa06372327cde84b79fe84b47
Kyle Bader <kyle.bader@dreamhost.com>
2012-02-01 16:00:48 -0800

    Metadata cleanups
    Change-Id: Ic7d9c7ceaa90a4bb6f211687e8e1a7a315d3eab1
    Reviewed-on: https://git.newdream.net/3994
    Tested-by: Gerrit Admin <admin@gerrit.newdream.net>
    Reviewed-by: Jordan Tardif <jordan.tardif@dreamhost.com>

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,4 @@ maintainer			"Kyle Bader"
 maintainer_email	"kyle.bader@dreamhost.com"
 license				"Apache 2.0"
 description			"Installs the parted disk utility"
-long_description▸   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version				"0.0.1"
